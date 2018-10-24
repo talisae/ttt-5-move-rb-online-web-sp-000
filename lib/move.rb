770d2cf3d17fed6094ea07f38942964246971124
Talisa Erazo <talisaerazo@gmail.com>
2018-10-24 15:58:02 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,11 +10,6 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(array, index, value = "X")
-  puts "#{value}"
-  puts array[index] = value
-end
-
 board = [" ", " ", " "]
 def update_array_at_with(array, index, value)
   array[index] = value
