8131fc9e37deaa213e722c62fd7df934e640663e
Talisa Erazo <talisaerazo@gmail.com>
2018-10-24 15:54:34 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,5 @@
-def display_board(board)
+def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -11,7 +12,6 @@ def input_to_index(user_input)
 end
 
 def move(array, index, value = "X")
-  board = [" ", " ", " "]
   puts "#{value}"
   puts array[index] = value
   return
